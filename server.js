5e699656f7dfe509cf50ea03bcf2bb02cba8e867
hariprakashkhp <hariprakash.khp@gmail.com>
2024-04-06 23:20:32 +0530

    added styles

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/urlShortner");
 
 //middleware:
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", async (req, res) => {
     const data = await shortUrl.find();
@@ -15,11 +16,12 @@ app.get("/", async (req, res) => {
 })
 
 app.post("/short", (req, res) => {
+    console.log("imhit")
     shortUrl.create({full:req.body.url});
     res.redirect("/");
 })
 
-app.get("/:id", async (req, res) => {
+app.get("/redirect/:id", async (req, res) => {
     let data = await shortUrl.findOne({short: req.params.id});
     if(data === null) res.sendStatus(404);
     data.clicks++;
